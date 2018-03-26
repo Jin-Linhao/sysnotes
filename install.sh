c0c00dbaf0567f6ae956ae80fa5a244644c2e017
Jin-Linhao <jin_linhao@hotmail.com>
2018-03-26 14:32:20 -0400

    add kazam and unity tweak

--- install.sh
+++ install.sh
@@ -6,9 +6,17 @@ echo "Installing Sublime text3..."
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-text
+sudo apt-get install sublime-text -y
 
 echo "Installing Terminator..."
-sudo add-apt-repository ppa:gnome-terminator
+sudo add-apt-repository ppa:gnome-terminator -y
 sudo apt-get update
-sudo apt-get install terminator
+sudo apt-get install terminator -y
+
+echo "Installing Kazam..."
+sudo apt-add-repository ppa:sylvain-pineau/kazam -y
+sudo apt-get update
+sudo apt-get install kazam -y
+
+echo "Installing Unity Tweak Tool..."
+sudo apt-get install unity-tweak-tool
